2f4e79f8723d1d85aea6f779b6f109bcb4bef341
oplingerpaul-cloud <oplingerpaul@gmail.com>
2026-01-30 20:32:05 +0000

    update

--- ec2-instance/security-groups/sg.tf
+++ ec2-instance/security-groups/sg.tf
@@ -1,10 +1,10 @@
-resource  "aws_security_group" "web_sg"
-{
-    name= "lab9-web-sg-${var.project}"
-    description="security group for webserver"
-    vpc_id = var.vpc_id
-    tags={
-        name= "lab9-web-sg-${var.project}"
-    }
-     vpc_security_group_ids = var.web_sg
+resource "aws_security_group" "web_sg" {
+  name        = "lab9-web-sg-${var.project}"
+  description = "Security group for web server"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "lab9-web-sg-${var.project}"
+  }
 }
+
